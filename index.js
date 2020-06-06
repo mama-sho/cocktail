9ec22c6ee24a5f00bbccd5d24a9ad81a649f8a24
KazukiSadasue <kazuki.fx0915@gmail.com>
2020-06-06 12:24:56 +0900

    add client

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+  res.send({ hi: 'there' });
 });
+
+// 本番環境では、ビルドしたReactを読み込むようにする
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resoleve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
